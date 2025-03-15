191fdad40654af596fed6c11ab868d0ff0602be7
balhyo-younjisang <yjs12180825@gmail.com>
2025-03-15 14:42:24 +0900

    Initialized Repository

---
+++ lib/utilities/routes.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/screens/index.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings routeSettings) {
+    try {
+      switch (routeSettings.name) {
+        default:
+          return errorRoute(routeSettings);
+      }
+    } catch (_) {
+      return errorRoute(routeSettings);
+    }
+  }
+
+  static Route<dynamic> errorRoute(RouteSettings routeSettings) {
+    return MaterialPageRoute(
+      settings: routeSettings,
+      builder: (_) => ErrorScreen(),
+    );
+  }
+}
