8de9b9b172305a5f49927f4cd731516cb7df2238
balhyo-younjisang <yjs12180825@gmail.com>
2025-03-16 03:53:33 +0900

    chore: build.gradle.kts 수정

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,7 +7,17 @@ allprojects {
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
-
+subprojects {
+    afterEvaluate { project ->
+        if (project.plugins.hasPlugin("com.android.application") ||
+                project.plugins.hasPlugin("com.android.library")) {
+            project.android {
+                compileSdkVersion 34
+                buildToolsVersion "34.0.0"
+            }
+        }
+    }
+}
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
